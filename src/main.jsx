b18890f6ef2f6e20d4681ed981bd327f2c7564d1
BenDevgame247 <devgame247@gmail.com>
2025-08-29 22:48:45 +0700

    Chỉnh sửa App.jsx, main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import './index.css'
-import router from "./feature/Routes"
-import App from './App.jsx'
+import { router } from './feature/Routes/router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

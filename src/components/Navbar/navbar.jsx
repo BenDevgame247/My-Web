e4e22907cd4cc18d49cf70c4def414f544627d29
BenDevgame247 <devgame247@gmail.com>
2025-08-30 21:08:18 +0700

    Css navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,14 +1,40 @@
 import clsx from "clsx"
+import { Link } from "react-router"
+
+import logo from "../../assets/LogoDiscord.png"
 import styles from "./navbar.module.scss"
 
 export default function Navbar() {
     return (
         <nav className={clsx(styles.navbar)}>
-            <ul className={clsx(styles.menu)}>
-                <li>
-                    <a href="/">Trang chủ</a>
-                </li>
-            </ul>
+            {/* Menu + logo */}
+            <button className={clsx(styles.logoBtn)} aria-label="Mở menu">
+                <img src={logo} alt="logo" />
+            </button>
+
+            {/* search */}
+            <div className={clsx(styles.search)}>
+                <form role="search">
+                    <input type="text" placeholder="Tìm kiếm...."/>
+                </form>
+            </div>
+
+            {/* avatar user */}
+            <div className={clsx(styles.avatar)}>
+                <img src={logo} alt="Avatar" />
+            </div>
+
+            {/* Sub menu */}
+            <div className={clsx(styles.subMenu)}>
+                <ul>
+                    <li>
+                        <Link to = "/">Trang chủ</Link>
+                    </li>
+                    <li>
+                        <Link to = "tai-lieu">Tài liệu</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -5,7 +5,7 @@ import Navbar from "../../components/Navbar/navbar"
 export default function Home() {
     return (
         <section>
-            <h1>Hello</h1>
+            
         </section>
     )
 }

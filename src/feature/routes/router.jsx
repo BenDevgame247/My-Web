b0153bf4b7f271fc8fc1972f08fb6269e9b9ba19
BenDevgame247 <devgame247@gmail.com>
2025-08-30 17:38:09 +0700

    Route: thêm trang tài liệu, chỉnh sửa router.jsx

--- src/feature/routes/router.jsx
+++ src/feature/routes/router.jsx
@@ -1,13 +1,16 @@
 import { createBrowserRouter } from "react-router";
 
 import App from "../../App";
+import Documents from "../../pages/Documents/document";
+import Home from "../../pages/Home/home"
 
 export const router = createBrowserRouter([
     {
         path: "/", 
         element: <App />,
         children: [
-            {path: "/", element: <Home />}
+            {path: "/", element: <Home />},
+            {path: "tai-lieu", element: <Documents />},
         ]       
     }
 ])

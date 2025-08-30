cb6c3c32da5e0cdfe6085456281c39353d41a7da
BenDevgame247 <devgame247@gmail.com>
2025-08-30 17:49:49 +0700

    Pages: chỉnh sửa trang home

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,5 +1,7 @@
 import clsx from "clsx"
 
+import Layout from "../../components/Layout/layout"
+// import styles from "../../components/Layout/layout.module.scss"
 
 export default function Home() {
     return (

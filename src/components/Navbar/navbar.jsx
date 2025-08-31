0efc66f0e11624029f95f3b7dafc7115f8f0304f
BenDevgame247 <devgame247@gmail.com>
2025-08-31 21:12:17 +0700

    Chỉnh sửa layout.module.scss, thêm bottom navbar cho mobile trong navbar.jsx, chỉnh sửa css navbar.module.scss

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -6,25 +6,46 @@ import styles from "./navbar.module.scss"
 
 export default function Navbar() {
     return (
-        <nav className={clsx(styles.navbar)}>
-            {/* Menu + logo */}
-            <div className={clsx(styles.logo)}>
-                <img src={logo} alt="logo" />
-            </div>
+        <>
+            {/* Top navbar */}
+            <nav className={clsx(styles.topNav)}>
+                {/* Menu + logo */}
+                <div className={clsx(styles.logo)}>
+                    <img src={logo} alt="logo" />
+                </div>
 
-            {/* search */}
-            <div className={clsx(styles.search)}>
-                <form role="search">
-                    <input type="text" placeholder="Tìm kiếm...."/>
-                </form>
-            </div>
+                {/* search */}
+                <div className={clsx(styles.search)}>
+                    <form role="search">
+                        <input type="text" placeholder="Tìm kiếm...."/>
+                    </form>
+                </div>
 
-            {/* avatar user */}
-            <div className={clsx(styles.avatar)}>
-                <img src={logo} alt="Avatar" />
-            </div>
+                {/* avatar user */}
+                <div className={clsx(styles.avatar)}>
+                    <img src={logo} alt="Avatar" />
+                </div>
+            </nav>
 
-            {/* Menu */}
-        </nav>
+            {/* Bottom navbar */}
+            <nav className={clsx(styles.botNav)}>
+
+                {/* List */}
+                <ul className={clsx(styles.list)}>
+                    <li className="item-1">
+                        <Link to="/">Trang chủ</Link>
+                    </li>
+                    <li className="item-2">
+                        <Link to="tai-lieu">Tài liệu</Link>
+                    </li>
+                    <li className="item-3">
+                        <Link to="dien-dan">Diễn đàn</Link>
+                    </li>
+                    <li className="item-4">
+                        <Link to="bai-tap">Bài tập</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
     )
 }

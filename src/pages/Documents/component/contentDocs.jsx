04cb4884f1d77a4cb476653ceecc9c9ab47d22b7
BenDevgame247 <devgame247@gmail.com>
2025-09-09 11:23:21 +0700

    Edit: make sidebarList of contentDocs and subList of HTML document and add gettingStart.jsx

--- src/pages/Documents/component/contentDocs.jsx
+++ src/pages/Documents/component/contentDocs.jsx
@@ -1,12 +1,40 @@
 import clsx from "clsx"
-
+import { Link } from "react-router"
 
 import styles from "./contentDocs.module.scss"
+import GettingStart from "../contents/gettingStart"
 
 export default function ContentDocs() {
     return (
             <div className={clsx(styles.content)}>
-                <h3>123456</h3>
+
+                {/* Sidebar - List of link director to main documents */}
+                <aside className={clsx(styles.sidebar)}>
+                    <ul className={clsx(styles.sidebarList)}>
+                        <li>
+                            <Link to="getting-start">Bắt đầu</Link>
+                        </li>
+                        <li>
+                            <Link to="docs-c">Tài liệu C</Link>
+                        </li>
+                        <li className={clsx(styles.list)}>
+                            <Link to="docs-html">Tài liệu HTML</Link>
+                            <ul className={clsx(styles.subList)}>
+                                <li>
+                                    <Link to="">HTML là gì?</Link>
+                                </li>
+                                <li>
+                                    <Link to="">Attribute</Link>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </aside>
+
+                {/* Box Content - this is a main content of documents of programing language */}
+                <div className={clsx(styles.boxContent)}>
+                    <GettingStart/>
+                </div>
             </div>
     )
 }

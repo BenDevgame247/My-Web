d6a52b5c3d6405856ef57da533186ccff3276374
BenDevgame247 <devgame247@gmail.com>
2025-09-09 13:06:46 +0700

    Add more in router.jsx

--- src/feature/routes/router.jsx
+++ src/feature/routes/router.jsx
@@ -4,6 +4,7 @@ import App from "../../App";
 import Documents from "../../pages/Documents/document";
 import Home from "../../pages/Home/home"
 import Forum from "../../pages/Forum/forum";
+import ProgrammingConcepts from "../../pages/Documents/contents/concepts/programming/programming";
 
 export const router = createBrowserRouter([
     {
@@ -11,7 +12,13 @@ export const router = createBrowserRouter([
         element: <App />,
         children: [
             {index: true, element: <Home/>},
-            {path: "tai-lieu", element: <Documents/>},
+            {
+                path: "tai-lieu", 
+                element: <Documents/>,
+                children: [
+                    {path: "khai-niem-lap-trinh", element: <ProgrammingConcepts/>}
+                ]
+            },
             {path: "dien-dan", element: <Forum/>},
         ]       
     }
